5198c012a9b49353ed5da2e76a7fc18645401550
Inti Bryon <bryoninti@microsoft.com>
2023-10-03 19:16:35 -0400

    Added additional pages

---
+++ AzureOpenAI-SDK-App/EmbeddingObject.cs
@@ -0,0 +1,27 @@
+﻿namespace AzureOpenAI_SDK_App
+{
+    public class EmbeddingObject
+    {
+        public string text_value { get; set; }
+        public Data text_data { get; set; }
+        public double score { get; set; }
+        public string id { get; set; }
+        public string key { get; set; }
+        public EmbeddingObject(string text_value, Data text_data, double score, string id)
+        {
+            this.text_value=text_value;
+            this.text_data=text_data;
+            this.score=score;
+            this.id =id;
+        }
+        public EmbeddingObject() { }
+    }
+
+    public class Data
+    {
+        public string _object { get; set; }
+        public int index { get; set; }
+        public List<float> embedding { get; set; }
+        public Data() { }
+    }
+}
